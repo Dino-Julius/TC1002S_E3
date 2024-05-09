0034b4ff9df15c4f8557f8b610d7c88084a8b754
Julio Vivas <jcvmlego@gmail.com>
2024-05-09 15:40:14 +0000

    Filtro de Huracanes para técnicas estadísticas

--- retoAnalitica/codigo/CasoHuracanes-Equipo3.R
+++ retoAnalitica/codigo/CasoHuracanes-Equipo3.R
@@ -27,6 +27,9 @@ huracanes <- read.csv("CasoHuracanesCSV.csv")
 # Filtrar los datos del Océano Pacífico Norte
 huracanes_pacifico <- huracanes[huracanes$Ocean == "Pacific", ]
 
+# 3.3.1 Eliminar los datos que no son reales (-999 en Wind y Pressure)
+huracanes_pacifico <- huracanes_pacifico[!(huracanes_pacifico$Wind <= 0 | huracanes_pacifico$Pressure <= 0), ]
+
 # 3. Exploración de los datos --------------------------------------------
 # 3.1. Identificar el tipo de dato de tres columnas diferentes
 Name_class <- class(huracanes$Name)
@@ -57,31 +60,33 @@ huracanes_pacifico$Fecha <- as.Date(huracanes_pacifico$Fecha)
 huracanes_pacifico <- huracanes_pacifico[, -grep("Population", names(huracanes_pacifico))]
 huracanes_pacifico <- huracanes_pacifico[, !(names(huracanes_pacifico) == "Name")]
 
-# 3.3.1 Eliminar los datos que no son reales (-999 en Wind y Pressure)
-huracanes_pacifico <- huracanes_pacifico[!(huracanes_pacifico$Wind <= 0 | huracanes_pacifico$Pressure <= 0), ]
-
 # 3.3.2 Eliminar los posibles NA
 huracanes_pacifico <- na.omit(huracanes_pacifico)
 
 # Filtrar huracanes_pacifico para incluir solo los registros donde el estatus sea "HU"
-# huracanes_pacifico <- filter(huracanes_pacifico, Status == "HU")
+huracanes_pacifico_hu <- filter(huracanes_pacifico, Status == " HU")
 
 # Realizar técnicas estadísticas ------------------------------------------
 
 # 4.1 Muestra la MEDIA y el PROMEDIO de las variables : WIND y PRESSURE, CO2
-media_wind <- mean(huracanes_pacifico$Wind)
-media_wind
+media_wind_hu <- mean(huracanes_pacifico_hu$Wind)
+media_wind_hu
 
-media_pressure <- mean(huracanes_pacifico$Pressure)
-media_pressure
+media_pressure_hu <- mean(huracanes_pacifico_hu$Pressure)
+media_pressure_hu
 
-media_co2 <- mean(huracanes_pacifico$CO2)
-media_co2
+media_co2_hu <- mean(huracanes_pacifico_hu$CO2)
+media_co2_hu
 
 # 4.2 Indica los valores de los cuartiles de las variables : WIND, PRESSURE, CO2
-cuartiles_wind
-cuartiles_pressure
-cuartiles_co2
+cuartiles_wind_hu <- quantile(huracanes_pacifico_hu$Wind, probs = c(0.25, 0.5, 0.75))
+cuartiles_wind_hu
+
+cuartiles_pressure_hu <- quantile(huracanes_pacifico_hu$Pressure, probs = c(0.25, 0.5, 0.75))
+cuartiles_pressure_hu
+
+cuartiles_co2_hu <- quantile(huracanes_pacifico_hu$CO2, probs = c(0.25, 0.5, 0.75))
+cuartiles_co2_hu
 
 # 4.3 Emite conclusiones
 # Revisión de datos
